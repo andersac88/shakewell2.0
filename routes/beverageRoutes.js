c010c61e3bf5a8663be6915ce77eb5273a0636dc
Andrew Andersen <andersac88@gmail.com>
2020-12-11 21:36:07 -0600

    more

--- routes/beverageRoutes.js
+++ routes/beverageRoutes.js
@@ -4,9 +4,10 @@ module.exports = (app) => {
 	app.get('/api/beverages', async (req, res) => {
 		const beverages = await Beverages.find({});
 		res.send(beverages);
-    });
-    
-    app.post('/api/beverages', async (req, res) => {
-        Beverages.
-    })
+	});
+
+	app.post('/api/beverages', async (req, res) => {
+		const beverages = await Beverages.create(req.body);
+		res.send(beverages);
+	});
 };
